3f5a9f395e4aee117159856ae68e8509ffac4540
avulpakir <avulpakir19312015@gmail.com>
2025-07-23 13:49:03 +0530

    Add files via upload

---
+++ install_apps.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Update package list and install applications
+sudo apt update
+sudo apt install -y filezilla vlc audacity telegram-desktop idle
+
+# Install snap applications
+sudo snap install code --classic
+sudo snap install vivaldi
+sudo snap install freetube
+sudo snap install wps-office-all-lang-no-internet
