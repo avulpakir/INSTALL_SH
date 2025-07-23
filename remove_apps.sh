7609ab52c3d4b5a44167d15477044b796ca246ae
avulpakir <avulpakir19312015@gmail.com>
2025-07-23 08:41:42 +0000

    Add files via upload

---
+++ remove_apps.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Remove specified applications installed via apt
+sudo apt remove -y filezilla vlc audacity telegram-desktop idle
+
+# Remove specified applications installed via snap
+sudo snap remove code
+sudo snap remove vivaldi
+sudo snap remove freetube
+sudo snap remove wps-office-all-lang-no-internet
+
+# Clean up unused dependencies
+sudo apt autoremove -y
---
+++ remove_packages.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Remove specified packages
+sudo apt remove -y gigolo hexchat gnome-sudoku gnome-mines parole rhythmbox sgt-puzzles xfburn thunderbird transmission-gtk
+
+# Clean up unused dependencies
+sudo apt autoremove -y
